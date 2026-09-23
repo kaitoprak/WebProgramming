0ad612afd5bd2119677a4a8286b9e6fd14ea5c0a
kaitoprak <sabrunrifqi8@gmail.com>
2026-09-23 09:25:25 +0800

    Buat class Transaction

---
+++ Tugas 5/Transaction.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+class Transaction
+{
+    public function __construct(
+        private int $id,
+        private string $type,
+        private float $amount
+    ) {
+    }
+
+    public function process(): bool
+    {
+        $balance = $_SESSION['balance'] ?? 0.0;
+
+        return match ($this->type) {
+            'deposit' => $this->updateBalance($balance + $this->amount),
+            'withdraw' => $balance >= $this->amount
+                ? $this->updateBalance($balance - $this->amount)
+                : false,
+            default => false,
+        };
+    }
+
+    private function updateBalance(float $newBalance): bool
+    {
+        $_SESSION['balance'] = $newBalance;
+
+        return true;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getAmount(): float
+    {
+        return $this->amount;
+    }
+}
